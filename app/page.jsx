cd38df1b2656ef26ac42314c9170e5e1babe9c7a
Luana Fassini <luanafassini.564@gmail.com>
2023-11-23 13:49:16 -0300

    feat: create page cadastro

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,56 @@
+"use client"
+
 import Header from "./components/header/Header";
+import { useState } from "react";
+import Input from "./components/input/Input";
+import estilo from "./components/input/input.module.css";
 
 
 export default function Home() {
+
+  const vazio = '';
+
+  const [task, setTesk] = useState(vazio);
+  const [taskList, setTeskList] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setTeskList([...taskList, task]);
+      setTesk(vazio)
+    } else {
+      alert("Preencha Todos os Campos!")
+    }
+  }
+
   return (
-<>
-<Header></Header>
-</>    
-  )
+    <div className={estilo.body}>
+
+      <Header></Header>
+      <div className={estilo.box}>
+        <h1 className={estilo.h1}>Entre em Contato Conosco</h1>
+
+        <div className={estilo.inputs}>
+          <input type="text" placeholder='Email'
+            value={task}
+            className={estilo.input1}
+            onChange={(param) => setTesk(param.target.value)}
+          />
+          <input type="text" placeholder='Mensagem'
+            value={task}
+            className={estilo.input2}
+            onChange={(param) => setTesk(param.target.value)}
+          />
+
+          <button onClick={addTask} className={estilo.button}>Cadastrar</button>
+        </div>
+      </div>
+
+      {
+        taskList.map((task, index) => (
+          <Input index={index} task={task} />
+        ))
+      }
+
+    </div>
+  );
 }
