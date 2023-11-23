640d00a7ea44f93fb02004cce0efe7fbe61291b9
raissas2 <raissa.floriano@aluno.senai.br>
2023-11-23 14:49:08 -0300

    Feat: Finalização do footer

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/app/components/footer/Footer.module.css"
+import { FaGithub, FaInstagram } from "react-icons/fa";
+import { SiLinktree } from "react-icons/si";
+import { MdOutlineMail } from "react-icons/md";
+
 function Footer(){
 
         return(
-            <div className={styles.footer}>
-                <div>
-                    <Link className={styles.links} href={'/'}>Home</Link >
-                    <Link className={styles.links} href={'/'}>Sobre nós</Link>
-                    <Link className={styles.links} href={'/'}>Contato</Link>
-                    <Link className={styles.links} href={'/'}>Cadastrar</Link>
+            <div className={styles.main}>
+                <div className={styles.icons}>
+                    <FaGithub className={styles.icons1} fontSize={30} color={"white"}/>
+                    <SiLinktree className={styles.icons1} fontSize={30} color={"white"}/>
+                    <FaInstagram className={styles.icons1} fontSize={30} color={"white"}></FaInstagram>
+                    
                 </div>
-                <div className={styles.copyright}>
-                <Link className={styles.linkcadastro} href={'/'}>©2023 copyright</Link>
+                <div className={styles.camihos}>
+                    <Link className={styles.links} href={"/"}>HOME</Link>
+                    <Link className={styles.links} href={"/"}>SOBRE NÓS</Link>
+                    <Link className={styles.links} href={"/"}>CONTATO</Link>
+                    <Link className={styles.links} href={"/"}>CADASTRO</Link> 
+                </div>
+                <div className={styles.todosdireitosreservados}>
+                    <p className={styles.titulocopyright}>COPYRIGHT ©</p>
                 </div>
             </div>
         )
