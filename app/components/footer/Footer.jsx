7bc99fcc59a5d1dbca2e31e76d93c88dfe0b023d
raissas2 <raissa.floriano@aluno.senai.br>
2023-11-23 10:31:41 -0300

    feat: iniciando footer

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import styles from "@/app/components/footer/"
 
 function footer(){
-    return(
-        <footer className={styles.inicial}>
 
-            <div className={styles.image}>
-            <Image src={"/"} width={130} height={130}></Image>
-         </div>
-          <div className={styles.containerLinks}>
-          <p className={styles.titulo}>NOSSAS PÁGINAS</p>
-                <Link href={'/'} className={styles.links}>Insta</Link>
-                <Link href={'/'} className={styles.links}>Git</Link>
-          </div>
-          <div className={styles.copyright}>
-                <p className={styles.pcopyright}>©2023 copyright</p>
+        return(
+            <div className={styles.footer}>
+                <div>
+                    <Link className={styles.links} href={'/'}>Home</Link>
+                    <Link className={styles.links} href={'/'}>Sobre nós</Link>
+                    <Link className={styles.links} href={'/'}>Contato</Link>
+                </div>
+                <div className={styles.copyright}>
+                <Link className={styles.copyright} href={'/'}>©2023 copyright</Link>
+                </div>
             </div>
-        </footer>
-    )
-}
-export default footer;
+        )
+    } footer;
+
+    export default footer;
